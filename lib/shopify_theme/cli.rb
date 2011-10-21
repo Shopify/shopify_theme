fbca59589ba4a63602aa6ce6f4f229f703eddb7d
Christopher Lobay <hello@christopherlobay.com>
2011-10-21 10:15:12 -0400

    Added Growl support, Shopify logo and a bunch of other goodies

--- lib/shopify_theme/cli.rb
+++ lib/shopify_theme/cli.rb
@@ -6,16 +6,20 @@ require 'fileutils'
 require 'json'
 require 'fssm'
 require 'sass'
+require 'growl'
 
 $update_ignore ||= []
 
 module ShopifyTheme
   class Cli < Thor
     include Thor::Actions
-
+		include Growl
+		
     BINARY_EXTENSIONS = %w(png gif jpg jpeg eot svg ttf woff swf)
+    ASSET_EXTENSION = /\.s[ca]ss?$/
     IGNORE = %w(config.yml)
-
+    ICON = File.dirname(__FILE__) + '/images/shopify.png'
+		
     tasks.keys.abbrev.each do |shortcut, command|
       map shortcut => command.to_sym
     end
@@ -24,6 +28,8 @@ module ShopifyTheme
     def configure(api_key=nil, password=nil, store=nil)
       config = {:api_key => api_key, :password => password, :store => store}
       create_file('config.yml', config.to_yaml)
+	    say("Generated config.yml", :green)
+	    notify "config.yml", :title => 'Config Generated', :icon => ICON
     end
 
     desc "download FILE", "download the shops current theme assets"
@@ -49,18 +55,19 @@ module ShopifyTheme
     end
     
     desc "replace FILE", "completely replace shop theme assets with local theme assets"
-    method_option :quiet, :type => :boolean, :default => false
+    method_option :quiet, :type => :boolean, :default => true
     def replace(*keys)
 	    say("Are you sure you want to completely replace your shop theme assets? This is not undoable.", :yellow)
 	    if ask("Continue? (Y/N): ") == "Y"
 		    remote_assets = keys.empty? ? ShopifyParty.asset_list : keys
 	      remote_assets.each do |asset|
-	        delete_asset(asset, options['quiet'])
+	        delete_asset(asset, true)
 	      end
 	      local_assets = keys.empty? ? local_assets_list : keys
 	      local_assets.each do |asset|
-	        send_asset(asset, options['quiet'])
+	        send_asset(asset, true)
 	      end
+	      notify "#{remote_assets.size} Removed \n #{remote_assets.size} Uploaded", :title => 'Replaced Assets', :icon => ICON unless options['quiet']
 	      say("Done.", :green) unless options['quiet']
 		  end
     end
@@ -118,33 +125,44 @@ module ShopifyTheme
 
     def send_asset(asset, quiet=false)
       data = {:key => asset}
-      compile_asset(asset, quiet)
-	    
-      if (content = File.read(asset)).is_binary_data? || BINARY_EXTENSIONS.include?(File.extname(asset).gsub('.',''))
-        data.merge!(:attachment => Base64.encode64(content))
-      else
-        data.merge!(:value => content)
-      end
-
-      if ShopifyParty.send_asset(data).success?
-        say("Uploaded: #{asset}", :green) unless quiet
-      else
-        say("Error: Could not upload #{asset}", :red)
-      end
+      if compile_asset(asset, quiet)  
+	      if (content = File.read(asset)).is_binary_data? || BINARY_EXTENSIONS.include?(File.extname(asset).gsub('.',''))
+	        data.merge!(:attachment => Base64.encode64(content))
+	      else
+	        data.merge!(:value => content)
+	      end
+	
+	      if ShopifyParty.send_asset(data).success?
+	        say("Uploaded: #{asset}", :green) unless quiet
+	       	notify "#{asset}", :title => 'Uploaded Asset', :icon => ICON unless quiet
+	      else
+	        say("Error: Could not upload #{asset}", :red)
+	      end
+      end   
     end
 
 		def compile_asset(asset, quiet=false)
-	    if File.extname(asset) =~ /\.s[ca]ss$/
-				sass_engine = Sass::Engine.for_file(asset,{})
-				asset.gsub!(/\.s[ac]ss$/, '.css')
-				$update_ignore.push(asset)
-				File.open(asset, 'w') {|f| f.write(sass_engine.render)}
-		    say("Rendered SASS to #{asset}", :magenta) unless quiet
+	    if asset =~ ASSET_EXTENSION
+				begin
+					original_asset = asset
+					sass_engine = Sass::Engine.for_file(asset,{})
+					asset.gsub!(ASSET_EXTENSION, '.css')
+					$update_ignore.push(asset)
+					File.open(asset, 'w') {|f| f.write(sass_engine.render)}
+					notify "#{original_asset} => #{asset}", :title => 'Rendered SASS', :icon => ICON unless quiet
+					say("Rendered SASS: #{original_asset} => #{asset}", :magenta) unless quiet
+					true
+				rescue Sass::SyntaxError => e
+					notify "#{e.sass_filename}:#{e.sass_line} - \n #{e.message}", :title => 'Syntax Error', :icon => ICON unless quiet
+					say("#{e.sass_filename}:#{e.sass_line} - #{e.message}", :red) unless quiet
+					false
+				end
 		  end
+		  true
 		end
 
     def delete_asset(key, quiet=false)
-			return if File.extname(key) =~ /\.s[ca]ss$/
+			return if key =~ ASSET_EXTENSION
 			if ShopifyParty.delete_asset(key).success?
         say("Removed: #{key}", :green) unless quiet
       else
