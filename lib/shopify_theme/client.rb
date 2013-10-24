9918c784ba08a86646b34d8cd7d005032aa8735c
Chris Saunders <chris.saunders@shopify.com>
2013-10-24 21:04:40 +0000

    API Client code along with tests

--- lib/shopify_theme/client.rb
+++ lib/shopify_theme/client.rb
@@ -18,17 +18,51 @@ module ShopifyTheme
 
     class AbstractClient
       include HTTParty
+      attr_reader :parser
 
       def initialize(parser)
         @parser = parser
       end
 
       def configured?
-        self.class.get('').code == 200
+        self.class.head('/assets.json').code == 200
       end
 
       def asset_list
-        []
+        response = get('/assets.json', parser: parser)
+        assets = parse(response)['assets'].map {|asset| asset['key']}
+        assets.reject { |asset| assets.include? "#{asset}.liquid"}
+      end
+
+      def get_asset(asset)
+        response = get('/assets.json', query: {asset: {key: asset}}, parser: parser)
+        parse(response)['asset']
+      end
+
+      def send_asset(data)
+        put('/assets.json', body: {asset: data})
+      end
+
+      def delete_asset(key)
+        delete('/assets.json', body: {asset: {key: key}})
+      end
+
+      private
+
+      def get(*args)
+        self.class.get(*args)
+      end
+
+      def put(*args)
+        self.class.put(*args)
+      end
+
+      def delete(*args)
+        self.class.delete(*args)
+      end
+
+      def parse(response)
+        JSON.parse(response.body)
       end
     end
 
--- spec/unit/client_spec.rb
+++ spec/unit/client_spec.rb
@@ -7,7 +7,12 @@ module ShopifyTheme
   describe "Client" do
     include WebMock::API
     include MockResponses
-    def client(config)
+
+    after do
+      WebMock.reset!
+    end
+
+    def client(config=configuration)
       Client.new(config)
     end
 
@@ -17,29 +22,56 @@ module ShopifyTheme
 
     it "should be able to report if it is correctly configured" do
       response = {body: response('assets_list.json'), status: 200}
-      mock_request(:get, configuration, response)
-      client = client(configuration)
+      mock_request(method: :head, response: response)
       assert client.configured?, "A 200 response from the API should report the client as correctly configured"
     end
 
     it "should be able to make a request to get a list of all assets on Shopify" do
       response = {body: response('assets_list.json'), status: 200}
-      mock_request(:get, configuration, response)
-      assets = client(configuration).asset_list
+      mock_request(response: response)
+      assets = client.asset_list
       assert_equal 2, assets.length
     end
 
-    it "should be able to make a request for a single asset"
+    it "should be able to make a request for a single asset" do
+      response = {body: response('single_asset.json'), status: 200}
+      query = {'asset' => {'key' => 'templates/search.liquid'}}
+      mock_request(query: query, response: response)
 
-    it "should be able to make a request to upload an asset"
+      asset = client.get_asset('templates/search.liquid')
+      assert_equal 802, asset['size']
+    end
 
-    it "should be able to make a request to remove an asset"
+    it "should be able to make a request to upload an asset" do
+      response = {body: response('single_asset.json'), status: 200}
+      asset  = {asset: {value: 'updating my liquids'}}
+      mock_request(method: :put, response: response) do |request|
+        assert_equal 'asset[key]=templates%2Fsearch.liquid&asset[value]=updating%20my%20liquids', request.body
+      end
+      client.send_asset(key: 'templates/search.liquid', value: 'updating my liquids')
+    end
+
+    it "should be able to make a request to remove an asset" do
+      mock_request(method: :delete) do |request|
+        assert_equal 'asset[key]=templates%2Fsearch.liquid', request.body
+      end
+      client.delete_asset('templates/search.liquid')
+    end
 
 
-    def mock_request(method, config, response=nil, &request_validator)
+    def mock_request(opts = {}, &request_validator)
+      config = opts.fetch(:configuration, configuration)
+      response = opts[:response]
+      method = opts.fetch(:method, :get)
+      query = opts[:query]
+
       uri = config.shop_uri.gsub('https://', "https://#{config.api_key}:#{config.password}@")
-      stubbed_request = stub_request(method, uri)
-      stubbed_request = stubbed_request.with{|r| request_validator.call(r)}  if block_given?
+      stubbed_request = stub_request(method, "#{uri}/assets.json")
+      if request_validator
+        stubbed_request = stubbed_request.with{|r| request_validator.call(r)}
+      elsif query
+        stubbed_request = stubbed_request.with(query: query)
+      end
       stubbed_request.to_return(response) if response
     end
 
